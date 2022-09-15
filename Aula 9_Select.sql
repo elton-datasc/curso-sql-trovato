ea69e30e0886f1e4aaf062f8310acc83646fd8e3
Elton <elton-guilherme@hotmail.com>
2022-09-15 20:51:01 -0300

    aula 09 select

---
+++ Aula 9_Select.sql
@@ -0,0 +1,28 @@
+
+select * from dbo.Turmas;
+
+select Turmas.* from Turmas;
+
+SELECT id_turma, id_curso FROM dbo.Turmas;
+
+
+SELECT * FROM dbo.Turmas T;
+
+select T.id_turma,T.id_curso,t.data_inicio 
+	from dbo.Turmas T;
+
+select * from dbo.Cursos;
+
+SELECT C.data_cadastro, C.nome_curso
+	FROM dbo.Cursos C;
+
+
+-- Nomes personalizados para nossos cursos
+
+Select T.id_curso AS IDC,T.id_turma AS IDT,T.data_inicio AS "data comeco"
+	FROM dbo.Turmas T;
+
+--Segunda forma
+
+Select T.id_curso IDC,T.id_turma IDT,T.data_inicio "data comeco"
+	FROM dbo.Turmas T;
